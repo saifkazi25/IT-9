7286541df3ca718fdfc8bbc6771d730d1d905ae3
saifkazi25 <m.saifkazi@gmail.com>
2025-07-06 23:48:02 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-white text-black">
+      {/* Just testing a redeploy */}
       <h1 className="text-4xl font-bold">🎉 Fantasy Quiz is working!</h1>
     </main>
   );
 }
+
 
 
 
